eba5100527fc164d2b873c4ee8dcd5a2fe9b8ec2
piotr25691 <71488561+piotr25691@users.noreply.github.com>
2022-05-15 10:03:06 +0200

    use css for slideshow

--- assets/scripts.js
+++
@@ -1,8 +0,0 @@
-const images = ["bg1.png", "bg2.png", "bg3.png"]
-const slideshow = () => images.forEach((e, i) => {
-    setTimeout(() => {
-        imageDiv = document.querySelector(".bg");
-        imageDiv.style.background = `url('assets/slideshow/${e}') center/cover no-repeat`;
-    }, i*5000);
-});
-setInterval(slideshow, images.length*5000)
